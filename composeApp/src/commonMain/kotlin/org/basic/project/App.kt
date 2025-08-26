2ac7ec71fb36cfa0fbfc9bb52d7520614d6959eb
pablo herrera <p.herrera@alumno.edu.um.ar>
2025-08-26 17:41:00 -0300

    practica testField

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -4,15 +4,21 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,6 +28,9 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf("") }
+        var lastName: String by remember { mutableStateOf("") }
+
         Column (
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -29,15 +38,36 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Hola Mundo!"
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                }
             )
-            Text(
-                text = "Programacion 2"
+            TextField(
+                value = lastName,
+                onValueChange = {
+                    lastName = it
+                }
             )
-            Text(
-                text = "Android studio"
-            )
+
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            AnimatedVisibility(name.isNotEmpty()){
+                Text(
+                    text = "name $name",
+                    fontSize = 24.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+
+            AnimatedVisibility(lastName.isNotEmpty()) {
+                Text(
+                    text = "lastName $lastName",
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
